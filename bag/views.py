80ff2ae6f53246d143cec5fd6903c6b487e973f5
Joh201 <johngt632@gmail.com>
2021-12-21 13:08:16 +0000

    Add shoping bag app ,urls,templates

---
+++ bag/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def view_bag(request):
+    ''' A view that returns the bag contents page'''
+
+    return render(request, 'bag/bag.html')
