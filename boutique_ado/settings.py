6d9ed9ac8734edf3abf618e85164c2b8b068aace
Joh201 <johngt632@gmail.com>
2022-01-06 20:40:49 +0000

    Deployment part-1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +121,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# default configuaration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
